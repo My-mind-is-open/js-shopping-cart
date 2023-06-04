aa79abc71daa1455222ef1024a1595eb8f6e7979
Maksim <impulselife2018@mail.ru>
2023-06-04 17:08:32 +0600

    Output of goods and creation of local storage

---
+++ constants/catalog.js
@@ -0,0 +1,74 @@
+
+
+const CATALOG = [
+	{
+		id: 'el1',
+		name: 'GIANNIS IMMORTALITY 2',
+		img: 'https://i.ibb.co/pj4JDsz/img1.jpg',
+		price: 10990,
+
+	},
+	{
+		id: 'el2',
+		name: 'NIKE KYRIE FLYTRAP VI',
+		img: 'https://i.ibb.co/Kbpt5qH/img2.jpg',
+		price: 12490,
+
+	},
+	{
+		id: 'el3',
+		name: 'RENEW ELEVATE III',
+		img: 'https://i.ibb.co/gtzn0Lr/img3.jpg',
+		price: 14990,
+
+	},
+	{
+		id: 'el4',
+		name: 'NIKE RENEW',
+		img: 'https://i.ibb.co/znF11XJ/img4.jpg',
+		price: 21990,
+
+	},
+	{
+		id: 'el5',
+		name: 'BLAZER LOW `77',
+		img: 'https://i.ibb.co/bvGtGnX/img5.jpg',
+		price: 16490,
+
+	},
+	{
+		id: 'el6',
+		name: 'AIR FORCE 1',
+		img: 'https://i.ibb.co/pRY5jMS/img6.jpg',
+		price: 18490,
+
+	},
+	{
+		id: 'el7',
+		name: 'WMNS DUNK LOW',
+		img: 'https://i.ibb.co/kQP5Vm9/img7.jpg',
+		price: 18990,
+
+	},
+	{
+		id: 'el8',
+		name: 'DUNK LOW',
+		img: 'https://i.ibb.co/h1mJXXN/img8.jpg',
+		price: 12990,
+
+	},
+	{
+		id: 'el9',
+		name: 'DUNK LOW RETRO',
+		img: 'https://i.ibb.co/pvB0VCY/img9.jpg',
+		price: 23990,
+
+	},
+	{
+		id: 'el10',
+		name: 'ZOOM FREAK',
+		img: 'https://i.ibb.co/fCwFC3n/img10.jpg',
+		price: 15990,
+
+	},
+]
